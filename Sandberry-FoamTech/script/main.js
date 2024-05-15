379bc485cdc2b93d4d4184158017c29deeac42ae
Hemang Solanki <126587027+HemangSolanki111@users.noreply.github.com>
2024-05-15 22:58:37 +0530

    Add files via upload

---
+++ Sandberry-FoamTech/script/main.js
@@ -0,0 +1,88 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("header-scrolled");
+    } else {
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapes = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapes.classList.remove("show");
+    });
+});
+
+
+$(document).click(function (event) {
+    if (!$(event.target).is('.navbar-collapse *')) {
+        $('.navbar-collapse').collapse('hide');
+    }
+});
+
+$(document).ready(function() {
+    $('#closeNavbarBtn').click(function() {
+        $('#navbarNav').collapse('hide'); // Hides the navbar when close button is clicked
+    });
+});
+
+
+
+
+
+
+
+// Slider JS
+//step 1: get DOM
+// let nextDom = document.getElementById('next');
+// let prevDom = document.getElementById('prev');
+
+// let carouselDom = document.querySelector('.carousel');
+// let SliderDom = carouselDom.querySelector('.carousel .list');
+// let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+// let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+// let timeDom = document.querySelector('.carousel .time');
+
+// thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+// let timeRunning = 3000;
+// let timeAutoNext = 7000;
+
+// nextDom.onclick = function(){
+//     showSlider('next');    
+// }
+
+// prevDom.onclick = function(){
+//     showSlider('prev');    
+// }
+// let runTimeOut;
+// let runNextAuto = setTimeout(() => {
+//     next.click();
+// }, timeAutoNext)
+// function showSlider(type){
+//     let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+//     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+//     if(type === 'next'){
+//         SliderDom.appendChild(SliderItemsDom[0]);
+//         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+//         carouselDom.classList.add('next');
+//     }else{
+//         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+//         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+//         carouselDom.classList.add('prev');
+//     }
+//     clearTimeout(runTimeOut);
+//     runTimeOut = setTimeout(() => {
+//         carouselDom.classList.remove('next');
+//         carouselDom.classList.remove('prev');
+//     }, timeRunning);
+
+//     clearTimeout(runNextAuto);
+//     runNextAuto = setTimeout(() => {
+//         next.click();
+//     }, timeAutoNext)
+// }
